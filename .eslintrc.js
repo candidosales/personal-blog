c391312db1487caa894a466c1cd7cc3bbae78b63
candidosales <candidosales@users.noreply.github.com>
2021-06-17 03:32:19 +0000

    Prettified Code!

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended"
+    "plugin:@typescript-eslint/recommended",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
