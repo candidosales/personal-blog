cb16bf79881c821ff96d4a86103dc0bbbe91d9ee
candidosales <candidosg@gmail.com>
2021-09-11 15:07:24 -0600

    add footer to contact

--- components/topbar.tsx
+++ components/topbar.tsx
@@ -4,29 +4,11 @@ import {
   IconButton,
   Link,
   Tooltip,
-  Modal,
-  ModalBody,
-  ModalCloseButton,
-  ModalContent,
-  ModalHeader,
-  ModalOverlay,
   useDisclosure,
-  Stack,
-  Accordion,
-  AccordionItem,
-  AccordionButton,
-  AccordionPanel,
-  AccordionIcon,
-  Box,
 } from "@chakra-ui/react";
-import {
-  RiLinkedinBoxFill,
-  RiGithubFill,
-  RiMediumFill,
-  RiMailFill,
-  RiWhatsappFill,
-} from "react-icons/ri";
+import {RiLinkedinBoxFill, RiGithubFill, RiMediumFill} from "react-icons/ri";
 import Image from "next/image";
+import ModalContact from "./modal-contact";
 
 const Topbar = (): ReactElement => {
   const {isOpen, onOpen, onClose} = useDisclosure();
@@ -78,95 +60,7 @@ const Topbar = (): ReactElement => {
           </Link>
         </Tooltip>
       </div>
-      <Modal isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Get in touch 😃</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            <p>
-              It will be a pleasure to talk to you about: <br />
-            </p>
-            <Accordion allowMultiple>
-              <AccordionItem>
-                <h2>
-                  <AccordionButton>
-                    <Box flex="1" textAlign="left">
-                      💡 New project
-                    </Box>
-                    <AccordionIcon />
-                  </AccordionButton>
-                </h2>
-                <AccordionPanel pb={4}>
-                  I'm always interested in knowing your ideas, analyzing
-                  feasibility and we can even build together 😊.
-                </AccordionPanel>
-              </AccordionItem>
-
-              <AccordionItem>
-                <h2>
-                  <AccordionButton>
-                    <Box flex="1" textAlign="left">
-                      📚 Consultancy
-                    </Box>
-                    <AccordionIcon />
-                  </AccordionButton>
-                </h2>
-                <AccordionPanel pb={4}>
-                  I have experience in various markets such as e-commerce,
-                  finance, marketing, telecommunication, security and with that
-                  I can share some of my knowledge and help your project or
-                  idea.
-                </AccordionPanel>
-              </AccordionItem>
-              <AccordionItem>
-                <h2>
-                  <AccordionButton>
-                    <Box flex="1" textAlign="left">
-                      👨🏻‍🏫 Mentoring
-                    </Box>
-                    <AccordionIcon />
-                  </AccordionButton>
-                </h2>
-                <AccordionPanel pb={4}>
-                  I would like to hear your story and present the possibilities
-                  that computing and the technology market can offer your career
-                  😃.
-                </AccordionPanel>
-              </AccordionItem>
-            </Accordion>
-            <p>👋 Feel free to speak with me by</p>
-            <Stack direction="row" spacing={4} className="modal-contact">
-              <Link
-                href="mailto:candidosg@gmail.com"
-                isExternal
-                title="by E-mail ..."
-              >
-                <Button
-                  leftIcon={<RiMailFill />}
-                  colorScheme="whatsapp"
-                  variant="solid"
-                >
-                  E-mail
-                </Button>
-              </Link>
-              <Link
-                href="https://wa.me/13068800349"
-                isExternal
-                title="or WhatsApp"
-              >
-                <Button
-                  leftIcon={<RiWhatsappFill />}
-                  colorScheme="whatsapp"
-                  variant="solid"
-                >
-                  WhatsApp
-                </Button>
-              </Link>
-            </Stack>
-          </ModalBody>
-        </ModalContent>
-      </Modal>
+      <ModalContact isOpen={isOpen} onClose={onClose} />
     </header>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,171 +1,190 @@
-import React from "react";
+import React, {ReactElement} from "react";
 import Portfolio from "../components/portfolio";
 import Topbar from "../components/topbar";
-import {ExternalLinkIcon} from "@chakra-ui/icons";
+import {ArrowForwardIcon, ExternalLinkIcon} from "@chakra-ui/icons";
 import {extras, portfolio, volunteer, posts} from "../data/portfolios";
-import {Flex, Link, SimpleGrid} from "@chakra-ui/react";
+import ModalContact from "../components/modal-contact";
+import {
+  Flex,
+  Link,
+  SimpleGrid,
+  Text,
+  Box,
+  useDisclosure,
+} from "@chakra-ui/react";
 
-const Home = () => (
-  <div className="cover">
-    <div className="overlay">
-      <Topbar></Topbar>
+const Home = (): ReactElement => {
+  const {isOpen, onOpen, onClose} = useDisclosure();
 
-      <Flex
-        className="container-flex"
-        direction="column"
-        align="center"
-        maxW={{xl: "1200px"}}
-        m="0 auto"
-      >
-        <main>
-          <section className="about">
-            <p className="presentation">👋 Hi, my name is</p>
-            <h1>Cândido Sales.</h1>
-            <h2>
-              I build things for <br />
-              the web and mobile.
-            </h2>
-            <p className="about-description">
-              I'm Brazilian 🇧🇷 and a{" "}
-              <Link href="https://en.wikipedia.org/wiki/Saskatoon" isExternal>
-                Saskatoon-based 🇨🇦
-                <ExternalLinkIcon mx="2px" />
-              </Link>{" "}
-              software engineer who specializes in building (and occasionally
-              designing) exceptional digital experiences. Currently, I'm a
-              software engineer at{" "}
-              <Link href="https://www.vendasta.com/" isExternal>
-                Vendasta
-                <ExternalLinkIcon mx="2px" />
-              </Link>{" "}
-              focused on building accessible, human-centered products.
-            </p>
-          </section>
-          <section className="grid">
-            <div className="grid-header">
-              <h3>🧳 Portfolio</h3>
-              <p>Some open source projects that I've built and maintain</p>
-            </div>
-            <SimpleGrid
-              className="portfolios"
-              columns={{sm: 1, md: 2, lg: 3}}
-              gap={6}
-            >
-              {portfolio.map((p, index) => (
-                <Portfolio
-                  key={index}
-                  imageUrl={p.imageUrl}
-                  imageAlt={p.imageAlt}
-                  title={p.title}
-                  description={p.description}
-                  url={p.url}
-                  techs={p.techs}
-                />
-              ))}
-            </SimpleGrid>
-          </section>
-          <section className="grid">
-            <div className="grid-header">
-              <h3>✨ Extras</h3>
-              <p>
-                Other projects that I develop in my spare time to practice new
-                resources and technologies
+  return (
+    <div className="cover">
+      <div className="overlay">
+        <Topbar></Topbar>
+
+        <Flex
+          className="container-flex"
+          direction="column"
+          align="center"
+          maxW={{xl: "1200px"}}
+          m="0 auto"
+        >
+          <main>
+            <section className="about">
+              <p className="presentation">👋 Hi, my name is</p>
+              <h1>Cândido Sales.</h1>
+              <h2>
+                I build things for <br />
+                the web and mobile.
+              </h2>
+              <p className="about-description">
+                I'm Brazilian 🇧🇷 and a{" "}
+                <Link href="https://en.wikipedia.org/wiki/Saskatoon" isExternal>
+                  Saskatoon-based 🇨🇦
+                  <ExternalLinkIcon mx="2px" />
+                </Link>{" "}
+                software engineer who specializes in building (and occasionally
+                designing) exceptional digital experiences. Currently, I'm a
+                software engineer at{" "}
+                <Link href="https://www.vendasta.com/" isExternal>
+                  Vendasta
+                  <ExternalLinkIcon mx="2px" />
+                </Link>{" "}
+                focused on building accessible, human-centered products.
               </p>
-            </div>
-            <SimpleGrid
-              className="portfolios"
-              columns={{sm: 1, md: 2, lg: 3}}
-              gap={6}
-            >
-              {extras.map((p, index) => (
-                <Portfolio
-                  key={index}
-                  imageUrl={p.imageUrl}
-                  imageAlt={p.imageAlt}
-                  title={p.title}
-                  description={p.description}
-                  url={p.url}
-                  techs={p.techs}
-                />
-              ))}
-            </SimpleGrid>
-          </section>
-          <section className="grid">
-            <div className="grid-header">
-              <h3>🤝 Volunteer</h3>
-              <p>
-                Some projects that I develop and help to maintain voluntarily
-                that it is in accordance with principles that I believe such as
-                education, culture, art, public health and sustainability
-              </p>
-            </div>
-            <SimpleGrid
-              className="portfolios"
-              columns={{sm: 1, md: 2, lg: 3}}
-              gap={6}
-            >
-              {volunteer.map((p, index) => (
-                <Portfolio
-                  key={index}
-                  imageUrl={p.imageUrl}
-                  imageAlt={p.imageAlt}
-                  title={p.title}
-                  description={p.description}
-                  url={p.url}
-                  techs={p.techs}
-                />
-              ))}
-            </SimpleGrid>
-          </section>
-          <section className="grid">
-            <div className="grid-header">
-              <h3>📰 Blog</h3>
-              <p>Some posts ...</p>
-            </div>
-            <SimpleGrid
-              className="portfolios blog"
-              columns={{sm: 1, md: 2, lg: 3}}
-              gap={6}
-            >
-              {posts.map((p, index) => (
-                <Portfolio
-                  key={index}
-                  imageUrl={p.imageUrl}
-                  imageAlt={p.imageAlt}
-                  title={p.title}
-                  description={p.description}
-                  url={p.url}
-                  techs={[]}
-                />
-              ))}
-            </SimpleGrid>
-          </section>
-        </main>
+            </section>
+            <section className="grid">
+              <div className="grid-header">
+                <h3>🧳 Portfolio</h3>
+                <p>Some open source projects that I've built and maintain</p>
+              </div>
+              <SimpleGrid
+                className="portfolios"
+                columns={{sm: 1, md: 2, lg: 3}}
+                gap={6}
+              >
+                {portfolio.map((p, index) => (
+                  <Portfolio
+                    key={index}
+                    imageUrl={p.imageUrl}
+                    imageAlt={p.imageAlt}
+                    title={p.title}
+                    description={p.description}
+                    url={p.url}
+                    techs={p.techs}
+                  />
+                ))}
+              </SimpleGrid>
+            </section>
+            <section className="grid">
+              <div className="grid-header">
+                <h3>✨ Extras</h3>
+                <p>
+                  Other projects that I develop in my spare time to practice new
+                  resources and technologies
+                </p>
+              </div>
+              <SimpleGrid
+                className="portfolios"
+                columns={{sm: 1, md: 2, lg: 3}}
+                gap={6}
+              >
+                {extras.map((p, index) => (
+                  <Portfolio
+                    key={index}
+                    imageUrl={p.imageUrl}
+                    imageAlt={p.imageAlt}
+                    title={p.title}
+                    description={p.description}
+                    url={p.url}
+                    techs={p.techs}
+                  />
+                ))}
+              </SimpleGrid>
+            </section>
+            <section className="grid">
+              <div className="grid-header">
+                <h3>🤝 Volunteer</h3>
+                <p>
+                  Some projects that I develop and help to maintain voluntarily
+                  that it is in accordance with principles that I believe such
+                  as education, culture, art, public health and sustainability
+                </p>
+              </div>
+              <SimpleGrid
+                className="portfolios"
+                columns={{sm: 1, md: 2, lg: 3}}
+                gap={6}
+              >
+                {volunteer.map((p, index) => (
+                  <Portfolio
+                    key={index}
+                    imageUrl={p.imageUrl}
+                    imageAlt={p.imageAlt}
+                    title={p.title}
+                    description={p.description}
+                    url={p.url}
+                    techs={p.techs}
+                  />
+                ))}
+              </SimpleGrid>
+            </section>
+            <section className="grid">
+              <div className="grid-header">
+                <h3>📰 Blog</h3>
+                <p>Some posts ...</p>
+              </div>
+              <SimpleGrid
+                className="portfolios blog"
+                columns={{sm: 1, md: 2, lg: 3}}
+                gap={6}
+              >
+                {posts.map((p, index) => (
+                  <Portfolio
+                    key={index}
+                    imageUrl={p.imageUrl}
+                    imageAlt={p.imageAlt}
+                    title={p.title}
+                    description={p.description}
+                    url={p.url}
+                    techs={[]}
+                  />
+                ))}
+              </SimpleGrid>
+            </section>
+          </main>
 
-        <footer>
-          <p>
-            made by
-            <Link color="blue.500" href="https://nextjs.org/" isExternal>
-              nextjs
-            </Link>
-            and
-            <Link color="blue.500" href="https://vercel.com/" isExternal>
-              vercel
-            </Link>
-          </p>
-          <p>
-            <Link
-              color="blue.500"
-              href="https://twitter.com/candidosales"
-              isExternal
-            >
-              @candidosales
-            </Link>
-          </p>
-        </footer>
-      </Flex>
+          <footer>
+            <h1 className="footer-text" onClick={onOpen}>
+              Let's work together
+              <ArrowForwardIcon />
+            </h1>
+            <Box justifyContent="space-between" display="flex">
+              <p>
+                made by
+                <Link color="blue.500" href="https://nextjs.org/" isExternal>
+                  nextjs
+                </Link>
+                and
+                <Link color="blue.500" href="https://vercel.com/" isExternal>
+                  vercel
+                </Link>
+              </p>
+              <p>
+                <Link
+                  color="blue.500"
+                  href="https://twitter.com/candidosales"
+                  isExternal
+                >
+                  @candidosales
+                </Link>
+              </p>
+            </Box>
+            <ModalContact isOpen={isOpen} onClose={onClose} />
+          </footer>
+        </Flex>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Home;
