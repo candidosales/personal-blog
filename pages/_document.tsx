1dc8f26ebb1a891a5a06ea2034935f71c2bbce26
candidosales <candidosg@gmail.com>
2022-02-25 00:03:35 -0600

    improve typo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -58,6 +58,16 @@ class MyDocument extends Document {
           <link rel="shortcut icon" href="/pwa/icon-512.png" />
           <link rel="apple-touch-icon" href="/pwa/icon-512.png" />
           <link rel="manifest" href="/manifest.json" />
+          <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossorigin
+          ></link>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins&display=swap"
+            rel="stylesheet"
+          ></link>
           {/* Global Site Tag (gtag.js) - Google Analytics */}
           <script
             async
